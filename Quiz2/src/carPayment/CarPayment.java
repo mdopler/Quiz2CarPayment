d9f5f800eb52fcd0924cbb669b7aca7427757c0d
MichaelDopler <MichaelDopler@10.0.0.216>
2018-09-28 00:23:07 -0400

    final commit

--- Quiz2/src/carPayment/CarPayment.java
+++ Quiz2/src/carPayment/CarPayment.java
@@ -4,31 +4,51 @@ package carPayment;
 import java.lang.Math;
 
 public class CarPayment {
+	// initialize variables
 	
 	private double CarCost;
+	
 	private double DownPayment;
+	
 	private double LengthofLoaninMonths;
+	
 	private double InterestRate;
 	
+	// this will take in potential car payments
 	public CarPayment(double carCost, double downPayment, double lengthofLoaninMonths, double interestRate) {
+		
 		this.CarCost = carCost;
+		
 		this.DownPayment = downPayment;
+		
 		this.LengthofLoaninMonths = lengthofLoaninMonths;
+		
 		this.InterestRate = interestRate;
+		
 	}
-	
+	// calculates monthly payment
 	public double monthlyPayment() {
+		
 		double monthlyRate = this.InterestRate / 12;
-		double num = (monthlyRate * (Math.pow(1+monthlyRate, this.LengthofLoaninMonths)));
-		double denom = (Math.pow(1+monthlyRate, this.LengthofLoaninMonths) - 1);
-		double MonthlyPayment = (this.CarCost - this.DownPayment) * (num/denom);
-		return MonthlyPayment;
+		// numerator part of calculation formula
+		double numerator  = (monthlyRate * (Math.pow(1 + monthlyRate, this.LengthofLoaninMonths)));
+		// denominator part of calculation formula
+		double denominator = (Math.pow(1 + monthlyRate, this.LengthofLoaninMonths) - 1);
+		
+		double MonthlyPayment = (this.CarCost - this.DownPayment) * (numerator / denominator);
+		
+		return Math.round(MonthlyPayment * 100d) / 100d;
 	}
 	
+	// calculates total interest
 	public double totalInterest() {
-		double nm = this.monthlyPayment() * this.LengthofLoaninMonths;
-		double TotalInterest = nm - this.CarCost;
-		return TotalInterest;
+		
+		double totalAmountPaid = this.monthlyPayment() * this.LengthofLoaninMonths;
+		
+		double TotalInterest = totalAmountPaid - this.CarCost;
+		
+		return Math.round(TotalInterest * 100d) / 100d;
+		
 	}
 	
 
--- Quiz2/src/carPaymentTest/CarPaymentTest.java
+++ Quiz2/src/carPaymentTest/CarPaymentTest.java
@@ -14,8 +14,17 @@ public class CarPaymentTest {
 	public void monthlyPayment_Test1() {
 		
 		CarPayment testCar = new CarPayment(35000, 0, 60, 0.10);
+		System.out.println(testCar.monthlyPayment());
+		assertTrue(testCar.monthlyPayment() == 743.65);
 		
-		assertTrue(Math.round(testCar.monthlyPayment()) == Math.round(743.65));
+	}
+	
+	@Test
+	public void monthlyPayment_Test2() {
+		
+		CarPayment testCar = new CarPayment(42000, 5000, 72, 0.12);
+		System.out.println(testCar.monthlyPayment());
+		assertTrue(testCar.monthlyPayment() == 723.36);
 		
 	}
 	
@@ -24,9 +33,21 @@ public class CarPaymentTest {
 		
 		CarPayment testCar = new CarPayment(35000, 0, 60, 0.10);
 		System.out.println(testCar.totalInterest());
-		assertTrue(Math.round(testCar.totalInterest()) == Math.round(9618.79));
-		
+		assertTrue(testCar.totalInterest() == 9619.00);
+		// I am not sure how the total interest could be 9,618.79 if you are paying
+		// 743.65 for 60 months.  The total cost would work out to 743.65 * 60 = 44,619
+		// and the total cost of the car is 35000.  The total interest should be 
+		// 44619 - 35000 = 9,619 and not 9618.79, I think there was a rounding error when the
+		// problem was created
 	}
 	
+	@Test
+	public void totalInterest_Test2() {
+		
+		CarPayment testCar = new CarPayment(42000, 5000, 72, 0.12);
+		System.out.println(testCar.totalInterest());
+		assertTrue(testCar.totalInterest() == 10081.92);
+		
+	}
 
 }
